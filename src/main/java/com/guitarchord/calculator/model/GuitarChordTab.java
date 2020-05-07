e0b742729d4eb0fcc7dad789e38f6ed6c8a73aa6
Ryan Scott Nylander <nylander@udel.edu>
2020-05-07 16:49:21 -0400

    + GuitarChordTab model. Now the fun begins

--- src/main/java/com/guitarchord/calculator/model/GuitarChordTab.java
+++ src/main/java/com/guitarchord/calculator/model/GuitarChordTab.java
@@ -4,5 +4,14 @@ import lombok.Data;
 
 @Data
 public class GuitarChordTab {
+
+    private GuitarNote[] notes = new GuitarNote[6];
     
+    @Data
+    public class GuitarNote {
+        private String string;
+        private int fret; //TODO upper, lower bounds, muted
+        private boolean muted;
+    }
+
 }
